efb2414a904001a7f9675914a440a42d1e84dd4d
Aleksandr Staetsky <aleksandrstaetskiy@gmail.com>
2020-01-23 15:12:16 +0300

    tabs component added

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,1 +1,67 @@
+import Vue from "vue"
 
+Vue.component('tabs', {
+    template: `
+    <div>
+
+      <div class="tab__switch">
+        <a v-for="tab in tabs" 
+            href="#" 
+            class="tab__link" :class="{'tab__link--active' : tab.isActive}"
+            @click="chooseTab(tab)"
+            >
+            {{ tab.name }}
+            </a>
+      </div>
+
+      <div class="tab__panel">
+        <slot></slot>
+      </div>
+
+    </div>
+    `,
+    data() {
+        return {
+            tabs: []
+        }
+    },
+
+    created() {
+        this.tabs = this.$children
+    },
+
+    methods: {
+        chooseTab(selectedTab) {
+            this.tabs.forEach(item => {
+                item.isActive = (item.name === selectedTab.name)
+            })
+        }
+    }
+})
+
+Vue.component('tab', {
+    template: `
+    <div v-show="isActive"> <slot></slot> </div>
+  `,
+    props: {
+        name: {
+            require: true
+        },
+        selected: {
+            default: false
+        }
+    },
+    data() {
+        return {
+            isActive: false
+        }
+    },
+
+    mounted() {
+        this.isActive = this.selected
+    }
+})
+
+new Vue({
+    el: '#tabs',
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 const config = require('./tasks/config');
 
 module.exports = {
-	mode: process.env.NODE_ENV,
-	entry: {
-		app: config.scripts.input,
-	},
-	output: {
-		filename: '[name].js',
-		path    : config.scripts.output
-	},
-	devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: '/node_modules/'
-			},
-		]
-	},
-	devServer: {
-		overlay: false
-	}
+    mode: process.env.NODE_ENV,
+    entry: {
+        app: config.scripts.input,
+    },
+    output: {
+        filename: '[name].js',
+        path: config.scripts.output
+    },
+    devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        }, ]
+    },
+    devServer: {
+        overlay: false
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' для webpack 1
+        }
+    }
 }
